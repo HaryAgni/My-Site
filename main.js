9fa4dd8d56a446e3c680254efe8079db04b44d5c
Harshit Agnihotri <haryagni123@gmail.com>
2024-10-12 00:17:45 +0530

    Auto-generated commit [ci skip]

--- main.js
+++ main.js
@@ -1422,7 +1422,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    trackAnalyticID: "G-XE9X54XCMH"
+    trackAnalyticID: "G-T57400BY5S"
 };
 /*
  * For easier debugging in development mode, you can import the following file
